b2a298f511b7625619f952b119d61e55db8c3c29
oksuide <tagwaylander223@gmail.com>
2024-11-09 01:51:35 +0200

    Добавлены функции работы с постами

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -5,12 +5,14 @@ import (
 	"apiForSN/models"
 	"errors"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
+// Блок работы с юзером
 func CreateUser(c *gin.Context) {
 	var user models.User
 	// Привязываем JSON-данные из тела запроса к переменной user
@@ -49,7 +51,6 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	// Логика для получения пользователя
 	// Получаем ID пользователя из параметров URL
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
@@ -75,10 +76,22 @@ func GetUser(c *gin.Context) {
 	})
 }
 
+func newFunction(c *gin.Context) (models.User, bool) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return models.User{}, true
+	}
+	return user, false
+}
+
 func UpdateUser(c *gin.Context) {
-	// Логика для обновления пользователя
 	// Получаем ID пользователя из контекста
-	userID := c.MustGet("userID").(int)
+	userID, exists := c.Get("userID")
+	if !exists {
+		c.JSON(401, gin.H{"error": "User not authenticated"})
+		return
+	}
 
 	// Привязываем JSON с изменениями к структуре
 	var updateData struct {
@@ -139,7 +152,11 @@ func UpdateUser(c *gin.Context) {
 func DeleteUser(c *gin.Context) {
 	// Логика для удаления пользователя
 	// Получаем ID пользователя из контекста
-	userID := c.MustGet("userID").(int)
+	userID, exists := c.Get("userID")
+	if !exists {
+		c.JSON(401, gin.H{"error": "User not authenticated"})
+		return
+	}
 	// Ищем пользователя в базе данных по ID
 	var user models.User
 	if err := db.DB.Where("id = ?", userID).First(&user).Error; err != nil {
@@ -164,4 +181,157 @@ func DeleteUser(c *gin.Context) {
 func hashPassword(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
 	return string(bytes), err
+}
+
+// Блок работы с постами
+func NewPost(c *gin.Context) {
+	var post models.Post
+	// Привязываем JSON-данные из тела запроса к переменной post
+	if err := c.ShouldBindJSON(&post); err != nil {
+		c.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+
+	// Устанавливем текущие дата/время для поста
+	post.Date = int(time.Now().Unix())
+	// Устанавливаем userID из контекста
+	userID, exists := c.Get("userID")
+	if !exists {
+		c.JSON(401, gin.H{"error": "User not authenticated"})
+		return
+	}
+	post.UserID = userID.(int)
+
+	// Сохраняем пост в базе данных
+	if err := db.DB.Create(&post).Error; err != nil {
+		c.JSON(500, gin.H{"error": "Error creating post"})
+		return
+	}
+
+	// Возвращаем успешный ответ с данными о созданном посте
+	c.JSON(201, gin.H{
+		"id":      post.ID,
+		"user_id": post.UserID,
+		"date":    post.Date,
+		"content": post.Content,
+	})
+}
+
+func DeletePost(c *gin.Context) {
+	// Получаем ID поста и ID пользователя из контекста
+	postID, postExists := c.Get("postID")
+	userID, userExists := c.Get("userID")
+	if !postExists || !userExists {
+		c.JSON(400, gin.H{"error": "Invalid request data"})
+		return
+	}
+	// Ищем пост в базе данных по ID
+	var post models.Post
+	if err := db.DB.Where("id = ?", postID).First(&post).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			// Пост с таким id не найден
+			c.JSON(404, gin.H{"error": "Post not found"})
+			return
+		} else {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+	}
+	// Проверяем автора и удаляем пост
+	if userID == post.UserID {
+		if err := db.DB.Delete(&post).Error; err != nil {
+			c.JSON(500, gin.H{"error": "Failed to delete user"})
+			return
+		}
+		c.JSON(200, gin.H{"message": "Post deleted successfully"})
+	} else {
+		c.JSON(403, gin.H{"error": "You must be author of post"})
+	}
+}
+
+func UpdatePost(c *gin.Context) {
+	// Получаем ID поста и ID пользователя из контекста
+	postID, postExists := c.Get("postID")
+	userID, userExists := c.Get("userID")
+	if !postExists || !userExists {
+		c.JSON(400, gin.H{"error": "Invalid request data"})
+		return
+	}
+
+	// Ищем пост в базе данных по ID
+	var post models.Post
+	if err := db.DB.Where("id = ?", postID).First(&post).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(404, gin.H{"error": "Post not found"})
+			return
+		}
+		c.JSON(500, gin.H{"error": "Database error"})
+		return
+	}
+
+	// Проверяем, что пользователь является автором поста
+	if userID != post.UserID {
+		c.JSON(403, gin.H{"error": "You must be the author of the post to update it"})
+		return
+	}
+
+	// Привязываем JSON с изменениями к структуре
+	var updateData struct {
+		Content string `json:"content,omitempty"`
+	}
+	if err := c.ShouldBindJSON(&updateData); err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request body"})
+		return
+	}
+
+	// Проверяем, что новое содержание поста не пустое
+	if updateData.Content == "" {
+		c.JSON(400, gin.H{"error": "New post content can't be empty"})
+		return
+	}
+
+	// Обновляем контент и дату поста
+	updates := map[string]interface{}{
+		"content": updateData.Content,
+		"date":    int(time.Now().Unix()),
+	}
+	if err := db.DB.Model(&post).Updates(updates).Error; err != nil {
+		c.JSON(500, gin.H{"error": "Failed to update post"})
+		return
+	}
+
+	// Обновляем значение `post` для возврата обновленных данных
+	post.Content = updateData.Content
+	post.Date = updates["date"].(int)
+
+	// Возвращаем успешный ответ с обновленными данными поста
+	c.JSON(200, gin.H{
+		"content": post.Content,
+		"date":    post.Date,
+	})
+}
+
+func FindPost(c *gin.Context) {
+	// Получаем ID пользователя из параметров URL
+	postID, err := strconv.Atoi(c.Param("postID"))
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid post ID"})
+		return
+	}
+	// Проверяем наличие пользователя с таким ID
+	var existingPost models.Post
+	if err := db.DB.Where("id = ?", postID).First(&existingPost).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(404, gin.H{"error": "Post not found"})
+			return
+		} else {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+	}
+	c.JSON(200, gin.H{
+		"id":       existingPost.ID,
+		"nickname": existingPost.Content,
+		"userID":   existingPost.UserID,
+	})
 }
